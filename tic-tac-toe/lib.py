6209cafa57048ba65aa3c5251e8a32aba332bbe8
Ari Koro <45109939+Agamer12@users.noreply.github.com>
2025-11-09 22:10:44 -0800

    added function comments

--- tic-tac-toe/lib.py
+++ tic-tac-toe/lib.py
@@ -1,6 +1,11 @@
-def updateState(board, input, player):
+# change the state of the board to reflect a input by either player one or two
+def updateState(board, input, player) -> None:
   return
-def printBoard(board):
+
+# Print the state of the board to the terminal with formating
+def printBoard(board) -> None:
   return
-def checkWin(board):
+
+# return if the board is in a win state
+def checkWin(board) -> bool:
   return False
